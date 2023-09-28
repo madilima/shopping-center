ac9fd848a54ab48dcb4e34ec408e752ae24462c4
Madu <madulpereira28@hotmail.com>
2023-09-28 17:46:33 -0300

    Add Produto class

---
+++ src/Produto.java
@@ -0,0 +1,33 @@
+public class Produto {
+
+    private String nome;
+    private float preco;
+
+    public Produto(String nome, float preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public String toString() {
+        String string = "Nome: " + this.nome + "\n";
+        string += "Preço: R$ " + this.preco + "\n";
+
+        return string;
+    }
+}
